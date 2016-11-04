669c8b52bb992a3ef8ea8df5723f5b9bf3f33d42
AlphaLFC <398505136@qq.com>
2016-11-04 16:15:07 +0800

    New implement, better accuracy!

--- tools/captcha_detect_flask.py
+++ tools/captcha_detect_flask.py
@@ -6,7 +6,8 @@ Created on Thu Oct 27 11:59:58 2016
 """
 
 from flask import Flask, request, render_template
-from captcha_detector import detect_captcha
+# from captcha_detector import detect_captcha
+from test import detect_captcha
 import os
 
 app = Flask(__name__)
@@ -15,10 +16,13 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         im = request.files['file']
-        path = os.path.join(os.getcwd(), im.filename)
+        path = os.path.join(os.getcwd(), 'static', im.filename)
         im.save(path)
-        det_str = 'Predict string is: {:s}'.format(detect_captcha(path))
-        return render_template('upload.html', results={'img': im.filename, 'str': det_str})
+        det_str, new_str = detect_captcha(path)
+        det_str = 'Old way prediction: ' + det_str
+        new_str = 'New way prediction: ' + new_str
+        return render_template('upload.html', results={'img': im.filename, 'str0': det_str,
+                                                       'str1': new_str})
     else:
         return render_template('upload.html')
 if __name__ == '__main__':
